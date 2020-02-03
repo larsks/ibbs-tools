2682ad69cc9602bf8dcdb330c6d1c57e08074a72
Lars Kellogg-Stedman <lars@oddbit.com>
2020-02-03 15:02:35 -0500

    Ignore default section of syncterm.lst

--- syncterm.py
+++ syncterm.py
@@ -7,21 +7,20 @@ class SynctermLst(object):
     re_option = re.compile(r'\s*(?P<name>\S+) *=(?P<value>.*)')
 
     def parse(self, fd):
-        cur = default = {}
+        cur = None
 
         for line in fd:
             mo = self.re_title.match(line)
             if mo:
-                if cur is not default:
+                if cur is not None:
                     yield(cur)
 
                 name = mo.group('name')
-                cur = dict(default)
-                cur['name'] = name
+                cur = {'name': name}
                 continue
             mo = self.re_option.match(line)
-            if mo:
-                name = mo.group('name').lower()
+            if mo and cur is not None:
+                name = mo.group('name').lower().strip()
                 value = mo.group('value')
                 cur[name] = value
 
